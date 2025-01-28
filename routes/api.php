51b328136dd7fa416152ec52eebd2b03f43c397d
Gary Clarke <garyclarke1974@gmail.com>
2025-01-28 18:08:17 +0000

    Add healthcheck endpoint and test

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php // routes/api.php
+
+declare(strict_types=1);
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/healthcheck', function (): JsonResponse {
+    return new JsonResponse(['status' => 'ok']);
+});
+
