8717792671f74cd51a65700959a9de6804e3160a
Gary Clarke <garyclarke1974@gmail.com>
2025-02-01 09:35:28 +0000

    Instantiate the webhook dto

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\DTOs\Webhook;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,17 @@ class WebhookController
 {
     public function __invoke(Request $request): JsonResponse
     {
+        // Determine the platform from header
+        $platform = strtolower($request->header('X-Webhook-Source', 'unknown'));
+
+        // Get the payload off of the request
+        $payload = $request->all();
+
+        // Instantiate the webhook DTO
+        $webhook = new Webhook($platform, $payload);
+
+        // ... do something with the webhook
+
         return new JsonResponse(status: Response::HTTP_NO_CONTENT);
     }
 }
