d4cefa9d810b30aec3d38ff1bc067b88bfb6f223
Gary Clarke <garyclarke1974@gmail.com>
2025-02-12 19:40:11 +0000

    Cast props and set fillables

--- app/Models/SubscriptionEvent.php
+++ app/Models/SubscriptionEvent.php
@@ -4,12 +4,35 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\DTOs\SubscriptionEventCategory;
 use Database\Factories\SubscriptionEventFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubscriptionEvent extends Model
 {
     /** @use HasFactory<SubscriptionEventFactory> */
     use HasFactory;
+
+    // fillables
+    protected $fillable = [
+        'subscription_provider_id',
+        'name',
+        'category',
+        'notification_type',
+        'in_trial',
+    ];
+
+    // casting
+    protected $casts = [
+        'in_trial' => 'boolean',
+        'category' => SubscriptionEventCategory::class,
+    ];
+
+    // relationship to sub provider
+    public function provider(): BelongsTo
+    {
+        return $this->belongsTo(SubscriptionProvider::class);
+    }
 }
