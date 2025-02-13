634b05ee8af68bcc7bd3933ac401fa6f94eb8e78
Gary Clarke <garyclarke1974@gmail.com>
2025-02-13 13:20:53 +0000

    Start seeders

---
+++ database/seeders/SubscriptionProviderSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubscriptionProvider;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubscriptionProviderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SubscriptionProvider::firstOrCreate(['name' => 'Apple']);
+
+        SubscriptionProvider::firstOrCreate(['name' => 'Google']);
+    }
+}
